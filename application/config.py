a4a2b22c9571d9bf8ac897a6584db1a37f6a4c69
THEODORA EGBUREDI <doraisioma@yahoo.com>
2026-07-14 05:27:48 +0100

    Add Docker Compose with PostgreSQL

--- application/config.py
+++ application/config.py
@@ -3,10 +3,14 @@ import os
 
 class Config:
 
-    SECRET_KEY = os.getenv("SECRET_KEY", "healthcare-secret-key")
+    SECRET_KEY = os.getenv(
+        "SECRET_KEY",
+        "healthcare-secret-key"
+    )
 
     SQLALCHEMY_DATABASE_URI = os.getenv(
-    "DATABASE_URL",
-    "sqlite:///healthcare.db"
-)
+        "DATABASE_URL",
+        "sqlite:///healthcare.db"
+    )
+
     SQLALCHEMY_TRACK_MODIFICATIONS = False
